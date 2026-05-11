10a722dde18cdfef20895f3d32d8ab78ace8ce3c
jstorra <jstorra.dev@gmail.com>
2026-05-11 11:43:01 -0500

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Mi Portfolio
+      </h1>
+    </div>
+  )
+}
+
+export default App
